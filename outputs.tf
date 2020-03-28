e61347ebf0e79875723570d349da81d76bbb6b9f
Jorge Castaneda <jorge_castaneda@sbcglobal.net>
2020-03-28 09:08:34 -0700

    updated output to display dynamodb

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,6 @@ output "config" {
       bucket = aws_s3_bucket.s3_bucket.bucket
       region = data.aws_region.current.name
       role_arn = aws_iam_role.iam_role.arn
+      dynamodb_table = aws_dynamodb_table.name
     }
 }
